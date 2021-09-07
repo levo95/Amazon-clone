e5f2e935d3a71578950cc1d0a873d93b829298ac
lingeshwaran1995@gmail.com <Lingeshwaran>
2021-09-07 18:54:09 +0530

    Starting out fresh from tutorial

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDJVy3HkVme8Oz6uYmdDgZfc69-KUFwPCM",
+    authDomain: "clone-bbdf5.firebaseapp.com",
+    projectId: "clone-bbdf5",
+    storageBucket: "clone-bbdf5.appspot.com",
+    messagingSenderId: "623517023176",
+    appId: "1:623517023176:web:1a7f7044582f62b64c9efd",
+    measurementId: "G-4NZLGLMBTC"
+  };
